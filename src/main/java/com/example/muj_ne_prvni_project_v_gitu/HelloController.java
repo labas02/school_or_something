06106b4da5ba3308dfded683e3b459c7bb50b2c1
labas02 <zdenda.cejka112@seznam.cz>
2024-06-11 09:48:26 +0200

    save

--- src/main/java/com/example/muj_ne_prvni_project_v_gitu/HelloController.java
+++ src/main/java/com/example/muj_ne_prvni_project_v_gitu/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.muj_ne_prvni_project_v_gitu;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Polygon;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private Polygon auto;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ColorPicker barva;
+
+
+    public void zmenBarvu(ActionEvent event) {
+        auto.setFill(barva.getValue());
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.muj_ne_prvni_project_v_gitu {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.muj_ne_prvni_project_v_gitu to javafx.fxml;
